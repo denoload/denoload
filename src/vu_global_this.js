6b8fc65f8e79635a75081b94a6502f049304bd52
Alexandre Negrel <negrel.dev@protonmail.com>
2023-10-11 09:54:45 +0200

    add custom user agent

--- src/vu_global_this.js
+++ src/vu_global_this.js
@@ -1,10 +1,23 @@
+import { VERSION } from "./version.ts";
+
 const realGlobalThis = {};
+
+const defaultUserAgent = `denoload/${VERSION}`;
+const defaultHeaders = {
+  "User-Agent": defaultUserAgent,
+};
 
 export function setup() {
   realGlobalThis.fetch = globalThis.fetch;
-  globalThis.fetch = function (...args) {
+  globalThis.fetch = function (resource, options) {
     const start = performance.now();
-    const promise = realGlobalThis.fetch(...args);
+
+    options = options || { headers: defaultHeaders };
+    options.headers = options.headers || defaultHeaders;
+    options.headers["User-Agent"] = options.headers["User-Agent"] ||
+      defaultUserAgent;
+
+    const promise = realGlobalThis.fetch(resource, options);
     promise.finally(() => {
       performance.measure("fetch", {
         start,
