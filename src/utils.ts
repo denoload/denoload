611a979bb7f352e193e9fee9c7aff9bf42adeb58
Alexandre Negrel <negrel.dev@protonmail.com>
2023-10-17 14:22:19 +0200

    fix duration formatting

--- src/utils.ts
+++ src/utils.ts
@@ -5,11 +5,11 @@ export function printMetrics (m: metrics.RegistryObj): void {
 
   for (const trendName in m.trends) {
     const trendObj = m.trends[trendName]
-    console.log(`  ${trendName}${'.'.repeat(24 - trendName.length)}: ${formatTrendTag(trendObj._)} total=${trendObj._.length}`)
+    console.log(`  ${pad(trendName, '.', 24)}: ${formatTrendTag(trendObj._)} total=${trendObj._.length}`)
 
     for (const [tagName, tag] of Object.entries(trendObj)) {
       if (tagName === '_') continue
-      console.log(`      ${tagName}${'.'.repeat(20 - tagName.length)}: ${formatTrendTag(tag)} total=${trendObj._.length}`)
+      console.log(`      ${pad(tagName, '.', 20)}: ${formatTrendTag(tag)} total=${trendObj._.length}`)
     }
   }
 
@@ -25,9 +25,9 @@ function formatTrendTag (trend: number[]): string {
   const p95Str = formatDuration(p95)
   const p99Str = formatDuration(p99)
 
-  return `avg=${avgStr}${' '.repeat(10 - avgStr.length)} min=${minStr}${' '.repeat(10 - minStr.length)}` +
-    `max=${maxStr}${' '.repeat(10 - maxStr.length)} p(90)=${p90Str}${' '.repeat(10 - p90Str.length)}` +
-    `p(95)=${p95Str}${' '.repeat(10 - p95Str.length)} p(99)=${p99Str}${' '.repeat(10 - p99Str.length)}`
+  return `avg=${pad(avgStr, ' ', 10)} min=${pad(minStr, ' ', 10)}` +
+    `max=${pad(maxStr, ' ', 10)} p(90)=${pad(p90Str, ' ', 10)}` +
+    `p(95)=${pad(p95Str, ' ', 10)} p(99)=${pad(p99Str, ' ', 10)}`
 }
 
 export function formatDuration (nanoseconds: number): string {
@@ -35,14 +35,18 @@ export function formatDuration (nanoseconds: number): string {
   const milliseconds = Math.floor(microseconds / 1000)
   const seconds = Math.floor(milliseconds / 1000)
 
-  let result = `${microseconds.toFixed(3)}μs`
-  if (milliseconds > 1) {
-    result = `${Math.floor(milliseconds)}.${Math.round(microseconds % 1000)}ms`
+  let result = `${microseconds}μs`
+  if (milliseconds > 0) {
+    result = `${milliseconds}.${microseconds % 1000}ms`
   }
 
-  if (seconds > 1) {
-    result = `${Math.floor(seconds)}.${Math.round(milliseconds % 1000)}s`
+  if (seconds > 0) {
+    result = `${seconds}.${milliseconds % 1000}s`
   }
 
   return result
+}
+
+function pad (str: string, p: string, size: number): string {
+  return `${str}${p.repeat(Math.max(size - str.length, 0))}`
 }
