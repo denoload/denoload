802f408d619aa1ab9f4d27f94dd05ce375704c45
Alexandre Negrel <negrel.dev@protonmail.com>
2023-10-21 08:50:12 +0200

    remove assert package

--- packages/assert/assert_greater_or_equal_test.ts
+++
@@ -1,9 +0,0 @@
-// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
-import { assertGreaterOrEqual, assertThrows } from './mod.ts'
-
-Deno.test('assertGreaterOrEqual', () => {
-  assertGreaterOrEqual(2, 1)
-  assertGreaterOrEqual(1n, 1n)
-
-  assertThrows(() => { assertGreaterOrEqual(1, 2) })
-})
